449c659dff305e213c51632cc87468fbd07f5635
羅紹賢 <samuello@iii.org.tw>
2022-10-18 17:46:23 +0800

    add some plots

--- plot.py
+++ plot.py
@@ -1,8 +1,15 @@
 import plotly.express as px
 import pandas as pd
+from datetime import timedelta
 
 
-df = pd.read_csv("./data/data.csv")
-fig = px.scatter(x=list(df.index), y=df["In_temperature"])
-fig.show()
-# fig.write_html("plot.html")
+df = pd.read_csv("./data/unlabeled-data.csv")
+df["Timestamp"] = pd.to_datetime(df["Timestamp"], format="%Y-%m-%d %H:%M:%S")
+df = df[(df["final thickness"] > 0) & (df["final thickness"] < 20)]
+ts_lst = ["2022-06-30", "2022-07-01", "2022-07-02", "2022-07-03", "2022-08-12", "2022-08-13", "2022-08-14"]
+ts = pd.to_datetime(ts_lst, format="%Y-%m-%d")
+for t_str, t in zip(ts_lst, ts):
+    df_ = df[(df["Timestamp"] > t) & (df["Timestamp"] < t + timedelta(days=1))]
+    fig = px.scatter(x=list(df_.index), y=df_["final thickness"])
+    # fig.show()
+    fig.write_html("./plot/{}.html".format(t_str))
